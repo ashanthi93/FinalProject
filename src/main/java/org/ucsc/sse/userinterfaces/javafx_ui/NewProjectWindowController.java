9801efa9fce697bb50ec92ec4726b531e02f717d
SChamal <cham.saccha@gmail.com>
2017-09-11 18:44:58 +0530

    NewProjectWindow finished

--- src/main/java/org/ucsc/sse/userinterfaces/javafx_ui/NewProjectWindowController.java
+++ src/main/java/org/ucsc/sse/userinterfaces/javafx_ui/NewProjectWindowController.java
@@ -1,14 +1,18 @@
 package org.ucsc.sse.userinterfaces.javafx_ui;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
 import com.jfoenix.controls.JFXRadioButton;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Preloader.StateChangeNotification.Type;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 
 public class NewProjectWindowController implements Initializable {
 
@@ -17,6 +21,9 @@ public class NewProjectWindowController implements Initializable {
     
     @FXML
     private JFXRadioButton threatCheck;
+    
+    @FXML
+    private JFXButton cancelBtn;
     
     @FXML
     private void threatCheckAction(ActionEvent event) throws Exception {
@@ -33,16 +40,30 @@ public class NewProjectWindowController implements Initializable {
     }
     
     @FXML
-    private void addBtnction(ActionEvent event) throws Exception {
+    private void addBtnAction(ActionEvent event) throws Exception {
         if(threatCheck.isSelected()){
-            fileOpen("Select Threat Reports");
+            fileOpen("Select Threat Reports", "HTM Files (*.htm)", "*.htm");
         }else if(bugCheck.isSelected()){
-            fileOpen("Select Static Code Analysis Reports");
+            fileOpen("Select Static Code Analysis Reports", "XML Files (*.xml)", "*.xml");
+        }else{
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText(null);
+            alert.setContentText("\n    Please select an option first!");
+            alert.showAndWait();
         }
     }
     
-    private void fileOpen(String title){
+    @FXML
+    private void cancelBtnAction(ActionEvent event) throws Exception {
+        Stage stage = (Stage)cancelBtn.getScene().getWindow();
+        stage.close();
+    }
+    
+    private void fileOpen(String title, String displayName, String fileType){
         FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(displayName, fileType);
+        fileChooser.getExtensionFilters().add(extFilter);
         Stage stage = new Stage();
         fileChooser.setTitle(title);
         fileChooser.showOpenDialog(stage);

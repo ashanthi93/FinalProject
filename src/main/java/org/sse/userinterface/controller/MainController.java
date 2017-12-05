be0d56cf52fffc364b9da8d20f48be8683dd6525
CHAMPC\ChamPC <cham.saccha@gmail.com>
2017-12-05 17:06:33 +0530

    Report input windows validated upto the opening of HomeWindow

--- src/main/java/org/sse/userinterface/controller/MainController.java
+++ src/main/java/org/sse/userinterface/controller/MainController.java
@@ -16,6 +16,8 @@ import org.sse.source.BugCategoriesLoader;
 
 public class MainController implements Initializable {
 
+    public static Scene newProjectWindow;
+
     @FXML
     private void settingsButtonAction(ActionEvent event) throws Exception {
 
@@ -47,12 +49,12 @@ public class MainController implements Initializable {
 
         Parent root = FXMLLoader.load(getClass().getResource(path));
         Stage stage = new Stage();
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
+        newProjectWindow = new Scene(root);
+        newProjectWindow.getStylesheets().add("/styles/Styles.css");
 
         stage.setTitle(title);
         stage.setResizable(resizable);
-        stage.setScene(scene);
+        stage.setScene(newProjectWindow);
         stage.show();
     }
     
